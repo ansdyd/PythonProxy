88ff6efd164b40b195d09dac2ecf40939e2fb487
MunYong <mjang@princeton.edu>
2017-06-01 23:16:14 -0400

    started on creating the start function

--- simple_proxy.py
+++ simple_proxy.py
@@ -16,7 +16,13 @@ buffer_size = 8192 # the max of the socket buffer size
 def start():
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket to be used
-		
+		s.bind('', listen_port)
+		s.listen(max_conn) # connections limited by the maximum number of connections supported
+		print('Listening Successfully at %d' % (listen_port))
+	except Exception e:
+		print 'Failed to Initialize Socket'
+		sys.exit()
+
 
 
 
